fd0f9982fb38e514c113587437d333064d41a91b
Dante <101451298+DanteEmery@users.noreply.github.com>
2022-08-13 20:38:22 -0700

    update proj2

--- proj2/main.cpp
+++ proj2/main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main () {
+    int num;
     cout << "Hello from Proj2" << endl;
+    cout << "Enter your favorite Number";
+    cin >> num;
+    cout << "Your favorite number is " << num << "?" << endl;
     return 0;   
 } 

ad7bd40851356bd226adc8d298d99e5c80be640f
Dante <101451298+DanteEmery@users.noreply.github.com>
2022-08-15 12:09:24 -0700

    + bmi calculation

--- bmiCalculator/main.cpp
+++ bmiCalculator/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void main () {
+int main() {
 
     //BMI Calculator
     //weight(kg) / height * height(m)
@@ -9,5 +9,26 @@ void main () {
     //Normal weight 185.-24.9
     //Overweight > 25 
 
+    float weight, height, bmi;
 
+    cout << "Weight(kg), Height(m)";
+
+    cin >> weight >> height;
+
+    bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        cout << "Underweight" << endl;
+    } 
+    else if (bmi > 25) {
+        cout << "Overweight" << endl;
+    }
+    else {
+        cout << "Normal Weight" << endl;
+    }
+
+    
+    cout << "Your bmi is " << bmi << endl;
+
+    return 0;
 }

49d6dad3e70bc7b72e5e3ab7ff4e0bb837790484
Dante <101451298+DanteEmery@users.noreply.github.com>
2022-08-15 11:49:46 -0700

    init Bmi Calc

---
+++ bmiCalculator/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void main () {
+
+    //BMI Calculator
+    //weight(kg) / height * height(m)
+    //Underweight < 18.5
+    //Normal weight 185.-24.9
+    //Overweight > 25 
+
+
+}
